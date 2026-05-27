d4d643542c820f98f509063327220d759b1f2467
Nevitha Linuk <nevithalinuk9@gmail.com>
2026-05-27 21:53:07 +0200

    found out some features in the dataset to do EDA

--- src/EDA.py
+++ src/EDA.py
@@ -4,8 +4,16 @@ import data_loader as dl
 
 df = dl.load_data()
 for col in df.columns:
-    #print(f"\n{df[col].value_counts()}")
-
+    print(f"\n{df[col].value_counts()}")
 
 print(df["Job_Growth_2030"].describe().round(2))
 print(df[["AI_Replacement_Risk", "Future_Demand_Score", "Performance_Score", "Job_Satisfaction"]].describe().round(2))
+
+"""
+Found out that,
+1. AI_Replacement_Risk -> 0.0 - 1.0 (1.0 being the highest)
+2. Future_Demand_Score -> 0.0 - 1.0 (1.0 being the highest)
+3. Performance Score -> 2.0 - 5.0 (5.0 being the highest)
+4. Job_Satisfaction -> 1.0 - 5.0 (5.0 being the highest)
+5. Required_Skills has multiple values per column therefore it needs to be exploded when analysing or visualizing
+"""
